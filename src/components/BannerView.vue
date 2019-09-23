6cda7b88ddbf0b50f76118590fd766c43557c6e2
Gerasim Gerasimov <dialix@yandex.ru>
2019-09-23 17:07:22 +0700

    карусель, теперь и на VueJS

--- src/components/BannerView.vue
+++ src/components/BannerView.vue
@@ -1,47 +1,57 @@
 <template>
   <div class="banners">
-    <transition-group name="slide" >
-    <div class="slider" v-for="uri in images"
-          :key="uri"
-          :style="{ backgroundImage: `url(${uri})` }">
+    <div class="slider">
+        <img :class="classPrev" :src="srcPrev" alt=""
+             :style="{ animationPlayState: playCtrl}">
+        <img :class="classNext" :src="srcNext" alt=""
+             :style="{ animationPlayState: playCtrl}" 
+             v-on:animationiteration = "AnimationIteration">  
     </div>
-    </transition-group>  
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
-/*
-      <div v-for="uri in images" :key="uri">
-          <img :src="uri" alt="">
-      </div>
-*/
 @Component
 export default class BannerView extends Vue {
   @Prop() private msg!: string;
-  private images: Array<string> = [ './banners/011.jpg',
-                                    './banners/012.jpg'];
-  private uriIndex: number = 0;
-  private uri: string = "";
+  private slides: string[] = [
+                      './banners/011.jpg',
+                      './banners/012.jpg',
+                      './banners/013.jpg',
+                      './banners/014.jpg',
+                      './banners/015.jpg',
+                      './banners/016.jpg'];
 
-  mounted(){
-    console.log("BannerView mounted");
-    this.update();
+  private classPrev: string = 'prev';
+  private classNext: string = 'next';
+  private srcPrev: string = './banners/011.jpg';
+  private srcNext: string = './banners/012.jpg';
+  private playCtrl: string = 'running'; // 'paused';
+
+  private AnimationIteration() {
+    const nextSlide: string = this.getNextSlide();
+    this.playCtrl = 'paused';
+    if (this.classNext === 'next') {
+      this.classNext = 'prev';
+      this.classPrev = 'next';
+      this.srcPrev = nextSlide;
+    } else {
+      this.classNext = 'next';
+      this.classPrev = 'prev';
+      this.srcNext = nextSlide;
+    }
+    setTimeout(() => {
+      this.playCtrl = 'running';
+    }, 2000);
   }
 
-  created(){
-      console.log("BannerView created");
+  private getNextSlide(): string {
+    this.slides = this.slides.concat(this.slides.splice(0, this.slides.length - 1));
+    return this.slides[0] || '';
   }
-  
-  private update(): void {
-    //console.log("update");
-    this.images = this.images.concat(this.images.splice(0, this.images.length - 1));
-    //console.log(this.images);
-    //if (++this.uriIndex >= this.images.length) this.uriIndex = 0;
-    //this.uri = this.images[this.uriIndex]; 
-    setTimeout(this.update, 2000);
-  }
+
 }
 </script>
 
@@ -53,48 +63,36 @@ export default class BannerView extends Vue {
     overflow: hidden;
 }
 
-.banners-image {
-    /*width: 100%;*/
-    height: 100%;
-}
-
-/*
-.slide-leave-active,
-.slide-enter-active {
-  transition: 1s;
-}
-.slide-enter {
-  transform: translate(100%, 0);
-}
-.slide-leave-to {
-  transform: translate(-100%, 0);
-}*/
-
-.slide-enter-active {
-  transition: 1s;
-}
-.slide-enter {
-  /*transform: translate(-100%, 0);*/
-}
-
-.slide-enter-to {
-  transform: translate(-100%, 0);
-}
-
 .slider{
-  overflow: hidden;
-  position: relative;
-  height: 100%;
-  width: 100%;
+    color: green;
+    border: 1px dashed blue;
+    width: 100%;
+    height: 300px;
+    overflow: hidden;
+    position: relative;
 }
 
-/*
-.img-slider img {
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right:0;
+@keyframes prev_animate {
+    0%   {left: 0%;}
+    100% {left: -100%;}
+  }
+  
+.prev {
+    position: absolute;
+    left: 0%;
+    width: 100%;
+    animation: prev_animate 3s ease-in-out infinite;
 }
-*/
+
+@keyframes next_animate {
+    0%   {left: 100%;}
+    100% {left: 0%;}    
+}
+
+.next {
+    position: absolute;
+    left: 100%;
+    width: 100%;
+    animation: next_animate  3s ease-in-out infinite;
+}
 </style>
